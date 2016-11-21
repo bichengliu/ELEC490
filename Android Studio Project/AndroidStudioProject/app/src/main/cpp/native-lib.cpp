a0f410119da9b84c62a2780a6dddaaf3b1a9a7dd
bichengliu <bicheng_liu@hotmail.com>
2016-11-20 21:37:03 -0500

    changes were made
    we can use helper functions now

--- Android Studio Project/AndroidStudioProject/app/src/main/cpp/native-lib.cpp
+++ Android Studio Project/AndroidStudioProject/app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,19 @@
 #include <jni.h>
 #include <string>
-#include <stdio.h>
+#include <sstream>
+
+extern "C"
+int addtwonum(int a, int b);
+
 
 extern "C"
 jstring
 Java_elec490_airphone_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++, the output is: ";
+    std::ostringstream val;
+    val << addtwonum(4,2);
+    std::string hello = "Hello from C++, the output is: " + val.str();
     return env->NewStringUTF(hello.c_str());
 }
 
@@ -15,5 +21,12 @@ extern "C"
 JNIEXPORT jint JNICALL
 Java_elec490_airphone_MainActivity_somefunc(JNIEnv *env, jobject instance, jint a, jint b) {
     jint c = a + b;
+    return c;
+}
+
+extern "C"
+int addtwonum(int a, int b)
+{
+    int c = a + b;
     return c;
 }
--- Android Studio Project/AndroidStudioProject/app/src/main/java/elec490/airphone/MainActivity.java
+++ Android Studio Project/AndroidStudioProject/app/src/main/java/elec490/airphone/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI()+Integer.toString(somefunc(1,2)));
+    tv.setText(stringFromJNI());
     }
 
     @Override
